722f694fa2f1b5b7e518c63835f46ffc13f25166
Chris Trahey <christrahey@gmail.com>
2021-03-15 21:56:08 -0700

    Initial comint

---
+++ README.md
@@ -0,0 +1,2 @@
+# Graviton + EKS walkthrough
+This repo holds simplified assets that demonstrate the use of Graviton 2 on EKS.
---
+++ main.py
@@ -0,0 +1,21 @@
+import platform, sys, time
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+arch = platform.machine()
+greeting = f"Hello, Snap! I'm {arch}\n"
+num_bytes = sys.getsizeof(greeting)
+
+
+class MyServer(BaseHTTPRequestHandler):
+  def do_GET(self):
+    self.send_response(200, "OK")
+    self.send_header('ContentLength', num_bytes)
+    self.end_headers()
+    self.flush_headers()
+    self.wfile.write(str.encode(greeting))
+
+
+if __name__ == "__main__":
+  httpd = HTTPServer(('0.0.0.0', 8000), MyServer)
+  httpd.serve_forever()
